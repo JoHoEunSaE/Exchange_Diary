12b21333973f41180c183551864c68d64938096d
sichoi42 <42.4.sichoi@gmail.com>
2024-04-19 23:57:37 +0900

    [BE] FIX: 서버 오류 알림 템플릿 수정

--- backend/src/main/java/org/johoeunsae/exchangediary/exception/utils/DiscordWebhookErrorSender.java
+++ backend/src/main/java/org/johoeunsae/exchangediary/exception/utils/DiscordWebhookErrorSender.java
@@ -9,31 +9,37 @@ import org.springframework.stereotype.Component;
 import org.springframework.web.client.RestTemplate;
 
 @Component
-public class DiscordWebhookErrorHandler {
+public class DiscordWebhookErrorSender {
 
 	@Value("${discord.webhook.url}")
 	private String DISCORD_WEBHOOK_URL;
 
-	private String formatErrorMessage(Exception exception, String requestUrl, String requestMethod) {
+	private String formatDefaultErrorMessage(Exception exception, String requestUrl,
+			String requestMethod) {
 		StringWriter sw = new StringWriter();
 		PrintWriter pw = new PrintWriter(sw);
 		exception.printStackTrace(pw);
 		String stackTrace = sw.toString();
-
 		// 첫 번째 에러 라인 추출
 		String firstLineOfStackTrace = stackTrace.split("\n")[0];
-
 		// 메시지 포맷팅
-		return String.format("**ERROR API URL**\n%s %s\n**MESSAGE**\n`%s`", requestMethod, requestUrl, firstLineOfStackTrace);
+		return String.format("**ERROR API URL**\n%s %s\n**MESSAGE**\n`%s`", requestMethod,
+				requestUrl, firstLineOfStackTrace);
 	}
 
-	public void sendErrorToDiscordWebhook(Exception exception, String requestUrl, String requestMethod) {
-		String message = formatErrorMessage(exception, requestUrl, requestMethod);
-
+	public void sendDefaultMessage(Exception exception, String requestUrl,
+			String requestMethod) {
+		String message = formatDefaultErrorMessage(exception, requestUrl, requestMethod);
 		RestTemplate restTemplate = new RestTemplate();
 		Map<String, String> body = new HashMap<>();
 		body.put("content", message);
+		restTemplate.postForEntity(DISCORD_WEBHOOK_URL, body, String.class);
+	}
 
+	public void sendWebErrorMessage(DiscordWebErrorMessage discordWebErrorMessage) {
+		RestTemplate restTemplate = new RestTemplate();
+		Map<String, String> body = new HashMap<>();
+		body.put("content", discordWebErrorMessage.toString());
 		restTemplate.postForEntity(DISCORD_WEBHOOK_URL, body, String.class);
 	}
 }
